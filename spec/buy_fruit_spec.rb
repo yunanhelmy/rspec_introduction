5059a504ea45796ef9f7e5be3c51b83c1c057531
Yunan Helmy <m.yunan.helmy@gmail.com>
2020-03-04 16:53:26 +0700

    Add falsey spec

--- spec/buy_fruit_spec.rb
+++ spec/buy_fruit_spec.rb
@@ -3,10 +3,18 @@ require './lib/buy_fruit'
 
 RSpec.describe BuyFruit do
   let(:wallet) { Wallet.new(1) }
-  let(:shopping) { BuyFruit.call(wallet, 9000) }
 
   it "should have brought fruit" do
+    shopping = BuyFruit.call(wallet, 9000)
+
     expect(shopping[:status]).to be_truthy
     expect(shopping[:wallet].balance).to eq(1000)
+  end
+
+  it "should have not brought fruit" do
+    shopping = BuyFruit.call(wallet, 12000)
+
+    expect(shopping[:status]).to be_falsey
+    expect(shopping[:wallet].balance).to eq(10000)
   end
 end
